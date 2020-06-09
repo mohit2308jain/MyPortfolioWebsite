3f37b4f3e907d942aeca9cd6f58ea0e433ebee31
Mohit Jain <mohit_pradeep@srmuniv.edu.in>
2020-06-09 20:11:10 +0530

    Update about.js

--- src/shared/about.js
+++ src/shared/about.js
@@ -6,3 +6,13 @@ export const ABOUT = `Currently pursuing B.Tech from SRM Institute of Science an
     Philatelist by Hobby. Loves to listen to music and explore new places.`;
 
 export const LINK = 'https://drive.google.com/file/d/1zI8NofxG5LZUibAS56xxIpQjOXWMfRM_/view?usp=sharing';
+
+export const EXP = {
+    title: 'Intern at Capgemini India.',
+    period: 'January-March 2020',
+    tasks: `Undergone a 3 month intensive training in the domain of 
+            Salesforce.com Administration and Development. Built
+            Certification Management System using tools provided by
+            Salesforce along with the user interface developed using
+            Lightning Web Framework of Salesforce.`
+}
