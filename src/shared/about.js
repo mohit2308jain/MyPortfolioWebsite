4823d6f392592d67e510846f034060c7c71f78eb
Mohit Jain <mohit_pradeep@srmuniv.edu.in>
2020-06-09 20:02:04 +0530

    Update about.js

--- src/shared/about.js
+++ src/shared/about.js
@@ -1,7 +1,7 @@
 export const SKILLS = ['React', 'Angular', 'JavaScript', 'Redux', 'Python', 'Java'];
 
-export const ABOUT = `Currently, Working as a Senior Analyst at Capgemini, India. 
-    Completed B.Tech from SRM Institute of Science and Technology. Develops web apps using React and Angular.
+export const ABOUT = `Currently pursuing B.Tech from SRM Institute of Science and Technology. 
+    Develops web apps using React and Angular.
     Also works on Salesforce.com Administration and Development projects.
     Philatelist by Hobby. Loves to listen to music and explore new places.`;
 
