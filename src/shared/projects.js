57162df7cf18901c2855ac675d897608b9ad0dc0
mohit2308jain <mohitjain5000@gmail.com>
2020-07-11 01:20:51 +0530

    Data Update

--- src/shared/projects.js
+++ src/shared/projects.js
@@ -2,6 +2,14 @@ export const PROJECTS =
     [
         {
             id: 1,
+            name:'Expenser',
+            year:'July 2020',
+            description:'An expense manager app build using React for front-end, PostgreSQL for database, and Node and Express for the back-end of the web app. The User can do add, update, delete the expenses. Also, the app is incorporated with charts for the analysis of expenses.',
+            links: ['https://expenser-react.vercel.app'],
+            labels: ['See Project']
+        },
+        {
+            id: 2,
             name:'Movies, Books, Food - All In One',
             year:'June 2020',
             description:'The web app allows users to search for their favorite movies, books, and food along with the weather information for the location of their preference. The app uses different public APIs for providing information to users. The app is built using both React and Angular as different versions of the app.',
@@ -9,7 +17,7 @@ export const PROJECTS =
             labels: ['React Link', 'Angular Link']
         },
         {
-            id: 2,
+            id: 3,
             name:'Like Youtube',
             year:'June 2020',
             description:'A single page web app clone of youtube video page. The app is built using React, Redux and Reactstrap is used for styling the web app along with youtube data API.',
@@ -17,7 +25,7 @@ export const PROJECTS =
             labels: ['See Project']
         },
         {
-            id: 3,
+            id: 4,
             name:'ToDo Web App Using MERN Stack',
             year:'May 2020',
             description:'A Todo web app built using React for front-end, MongoDB for database, Node and Express for back-end of the web app. The User can add the task with the category, due date and remarks for the task. The User can also delete a particular task and mark the task as completed.',
@@ -25,7 +33,7 @@ export const PROJECTS =
             labels: ['Github Link']
         },
         {
-            id: 4,
+            id: 5,
             name:'Certification Management Application',
             year:'Apr 2020',
             description:'The Certification Management Application is a salesforce-based application for the management of certifications and vouchers. The application is made using the tools such as process builder, approval process, apex triggers provided by salesforce.',
